89987846c4101ddfd89b156fe2569ecb866cf6e2
Jakub Piasecki <jakubpiasecki05@gmail.com>
2022-02-17 16:45:16 +0100

    Nice working ver, kinda similar to tutorial unfortunately

--- server.cpp
+++ server.cpp
@@ -1,3 +1,4 @@
+#include <boost/asio/registered_buffer.hpp>
 #include <boost/system/detail/error_code.hpp>
 #include <iostream>
 #include <boost/asio.hpp>
@@ -11,53 +12,45 @@ class Server{
 public:
     typedef boost::shared_ptr<tcp::socket> sock_ptr;
     Server(boost::asio::io_context &io_context) : ios(io_context)
-                                                , acceptor_(tcp::acceptor(io_context,tcp::v4()))
-                                                , socket_(tcp::socket(io_context))
+                                                , acceptor_(tcp::acceptor(io_context, tcp::endpoint(tcp::v4(), 13)))
     {
         std::cout<<"Server has been created!"<<std::endl;
     }
-    sock_ptr start(){
-        sock_ptr tmp = sock_gen(ios);
-        /* acceptor_.async_accept(socket_, */
-        /*                       boost::bind(&Server::AcHandler, this, boost::asio::placeholders::error) */
-        /*                       ); */
-        return tmp;
+    void start(){
+        sock_ptr sockt = sock_gen(ios);
+        acceptor_.async_accept(*sockt.get(),
+                              boost::bind(&Server::accept_handler, this, sockt, boost::asio::placeholders::error)
+                              );
     }
-    void AcHandler(const boost::system::error_code& er){
-        if(er)
-            std::cerr<<er.what()<<std::endl;
-        else
-            std::cout<<"Correct!";
-    }
+private:
     sock_ptr sock_gen(boost::asio::io_context &io){
         return sock_ptr(new tcp::socket(io));
     }
-private:
+    void accept_handler(sock_ptr sock, const boost::system::error_code& error){
+        if(!error){
+            std::cout<<"Connection Established!"<<std::endl;
+            std::cout<<"Sending message..."<<std::endl;  
+            boost::asio::async_write(*sock.get(),  boost::asio::buffer("Test message\n"), 
+                                    [](const boost::system::error_code& e, size_t bytes){if(!e) std::cout<<"Message has been sent!\n";}
+                                    );
+            (*sock.get()).close();
+        } else{
+            std::cerr<<error.what()<<std::endl;
+        }
+        start();
+            
+    }
     boost::asio::io_context& ios;
     tcp::acceptor acceptor_;
-    tcp::socket socket_;
 };
 
-void accept_handler(const boost::system::error_code& error){
-    if (!error){
-        /* x=1; */
-        /* std::cerr<<error.what()<<std::endl; */
-    /* else */
-        std::cout<<"Okay!"<<std::endl;
-    }
-}
 
 int main(){
     try{
         boost::asio::io_context io_con;
         Server server(io_con);
-        /* Server::sock_ptr client_sock = server.start(); */
-        tcp::socket sock(io_con);
-        tcp::acceptor acptr(io_con, tcp::endpoint(tcp::v4(), 13));
-        for(;;){
-            acptr.async_accept(sock, boost::bind(accept_handler, std::placeholders::_1));
-            io_con.run();
-        }
+        server.start();
+        io_con.run();
     }
     catch(boost::system::error_code &e){
         throw e.what();
