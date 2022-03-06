592b6745f44439411b2cc9fc497a5bb722022343
Jakub Piasecki <jakubpiasecki05@gmail.com>
2022-03-06 13:33:00 +0100

    forgot about hpp files

---
+++ header_files/connection.hpp
@@ -0,0 +1,37 @@
+#include <boost/asio.hpp>
+#include <exception>
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/array.hpp>
+
+#pragma once
+class Client;
+
+using boost::asio::ip::tcp;
+class Connection : public boost::enable_shared_from_this<Connection>{
+
+public:
+    typedef boost::shared_ptr<Connection> ptr;
+    Connection(boost::asio::io_context &io_context_);
+    tcp::socket& socket();
+    void read_opt();
+    void opt_handler(const boost::system::error_code &e, size_t);
+    void opt_executer();
+    bool send_all_clients(const boost::system::error_code &e, size_t, int l, std::string clients);
+    void send_msg_length(int l, std::string c);
+    void send_clients_handler(const boost::system::error_code &e, size_t b);
+    void get_msg();
+    void get_msg_length();
+    void get_msg_length_handler(const boost::system::error_code &e, size_t, boost::array<char, 4>* x);
+    static ptr create(boost::asio::io_context& io_context){
+        return ptr(new Connection(io_context));
+    }
+
+private:
+    tcp::socket socket_;
+    char option[1];
+};
+
